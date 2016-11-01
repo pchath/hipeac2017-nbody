d26f989cec34ffe4b71458b1a8993f30afd1d04b
Prasanth Chatarasi <pchath@outlook.com>
2016-11-01 17:09:22 +0000

    Reduced number of synchronization points and increased the coarse granularity

--- mic/nbody/nbody/nbody-par-opt.cpp
+++ mic/nbody/nbody/nbody-par-opt.cpp
@@ -14,16 +14,16 @@ void randomizeBodies_seq_opt(float *data, int n) {
 	}
 }
 
-void bodyForce_seq_opt(BodySystem p, float dt, int n, int tileSize) {
+void bodyForce_seq_opt(BodySystem* bodies, float dt, int n, int tileSize) {
 
-	for (int tile = 0; tile < n; tile += tileSize) {
-		int to = tile + tileSize;
-		if (to > n) to = n;
+	int nTiles = n / tileSize;
 #pragma omp parallel for schedule(static) num_threads(4)
+	for (int tile = 0; tile < nTiles; tile += 1) {
+		BodySystem p = bodies[tile];
 		for (int i = 0; i < n; i++) {
 			float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
 #pragma vector aligned
-			for (int j = tile; j < to; j++) {
+			for (int j = tile; j < tile + tileSize; j++) {
 				float dy = p.y[j] - p.y[i];
 				float dz = p.z[j] - p.z[i];
 				float dx = p.x[j] - p.x[i];
@@ -36,6 +36,16 @@ void bodyForce_seq_opt(BodySystem p, float dt, int n, int tileSize) {
 			p.vx[i] += dt*Fx; p.vy[i] += dt*Fy; p.vz[i] += dt*Fz;
 		}
 	}
+
+	BodySystem p = bodies[0];
+	for (int i = 0; i < n; i++) {
+		for (int tile = 1; tile < nTiles; tile++) {
+			BodySystem q = bodies[tile];
+			p.vx[i] += q.vx[i];
+			p.vy[i] += q.vy[i];
+			p.vz[i] += q.vz[i];
+		}
+	}
 }
 
 int main(const int argc, const char** argv) {
@@ -43,26 +53,31 @@ int main(const int argc, const char** argv) {
 	int nBodies = 32768;
 	if (argc > 1) nBodies = atoi(argv[1]);
 
-	int tileSize = 32;
+	int tileSize = 512;
 	if (tileSize > nBodies) tileSize = nBodies;
+	int nTiles = nBodies / tileSize;
 
 	const float dt = 0.01f; // time step
 	const int nIters = 20;  // simulation iterations
 
-	int bytes = nBodies*sizeof(BodySystem);
-	float *buf = (float*)mkl_malloc(bytes, 64);
-	BodySystem p;
-	p.x = buf + 0 * nBodies; p.y = buf + 1 * nBodies; p.z = buf + 2 * nBodies;
-	p.vx = buf + 3 * nBodies; p.vy = buf + 4 * nBodies; p.vz = buf + 5 * nBodies;
+	int offset = nBodies;
+	BodySystem* bodies = (BodySystem*)mkl_malloc(sizeof(BodySystem)*nTiles, 64);
+	for (int i = 0; i < nTiles; i++) {
+		int bytes = nBodies*sizeof(BodySystem);
+		float* buf = (float*)mkl_malloc(bytes, 64);
+		randomizeBodies_seq_opt(buf, 6 * nBodies); // Init pos / vel data
+		bodies[i].x = buf + 0 * offset; bodies[i].y = buf + 1 * offset; bodies[i].z = buf + 2 * offset;
+		bodies[i].vx = buf + 3 * offset; bodies[i].vy = buf + 4 * offset; bodies[i].vz = buf + 5 * offset;
+	}
 
-	randomizeBodies_seq_opt(buf, 6 * nBodies); // Init pos / vel data
 
 	double totalTime = 0.0;
 
 	for (int iter = 1; iter <= nIters; iter++) {
 		StartTimer();
 
-		bodyForce_seq_opt(p, dt, nBodies, tileSize); // compute interbody forces
+		bodyForce_seq_opt(bodies, dt, nBodies, tileSize); // compute interbody forces
+		BodySystem p = bodies[0];
 
 #pragma ivdep
 #pragma vector aligned
@@ -89,5 +104,7 @@ int main(const int argc, const char** argv) {
 	//	printf("%d Bodies: average %0.3f Billion Interactions / second\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
 	//#endif
 	printf("%d Bodies: average %0.3f Billion Interactions / second\n", nBodies, 1e-9 * nBodies * nBodies / avgTime);
-	mkl_free(buf);
+	for (int i = 0; i < nTiles; i++) {
+		mkl_free(bodies[i].x);
+	}
 }
